46ae0ce3ca54ecf53da5ca520ec56824c8812665
Kot-off <antoxa0994@mail.ru>
2026-02-24 14:08:05 +0300

    added hunter method

--- CI-CD/02-Teamcity/example-teamcity/src/main/java/plaindoll/HelloPlayer.java
+++ CI-CD/02-Teamcity/example-teamcity/src/main/java/plaindoll/HelloPlayer.java
@@ -1,9 +1,12 @@
 package plaindoll;
 
-public class HelloPlayer{
-	public static void main(String[] args) {
-		Welcomer welcomer = new Welcomer();
-		System.out.println(welcomer.sayWelcome());
-		System.out.println(welcomer.sayFarewell());
-	}
+public class HelloPlayer {
+    public String getWelcomeMessage() {
+        return "Welcome to the world of Bloodborne!";
+    }
+
+    // ТВОЙ НОВЫЙ МЕТОД ЗДЕСЬ
+    public String hunter() {
+        return "Good hunter, your presence soothes me.";
+    }
 }
