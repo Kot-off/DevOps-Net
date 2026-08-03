3006c331b191e23a2473e8e433b3e28658dcc585
Kot-off <antoxa0994@mail.ru>
2026-08-03 19:59:29 +0300

    git actions\var

--- devops-diplom-yandexcloud/terraform/variables.tf
+++ devops-diplom-yandexcloud/terraform/variables.tf
@@ -1,20 +1,24 @@
 variable "token" {
   type        = string
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+  default     = ""
 }
 
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  default     = ""
 }
 
 variable "folder_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  default     = ""
 }
 
 variable "default_zone" {
-  default     = "ru-central1-a"
+  type    = string
+  default = "ru-central1-a"
 }
 
 variable "subnets" {
