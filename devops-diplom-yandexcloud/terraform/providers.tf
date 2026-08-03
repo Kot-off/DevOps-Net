ebc8233ee4416c29f1fa210ab08d888b159dd4b7
Kot-off <antoxa0994@mail.ru>
2026-08-03 20:08:33 +0300

    fix

--- devops-diplom-yandexcloud/terraform/providers.tf
+++ devops-diplom-yandexcloud/terraform/providers.tf
@@ -22,8 +22,8 @@ terraform {
 }
 
 provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = "ru-central1-a"
+  service_account_key_file = "key.json"
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = "ru-central1-a"
 }
